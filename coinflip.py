a189ebf744a74d42a7c6c1a2e8cef0f8216edf55
Nett18 <kressckerl@gmail.com>
2017-10-29 14:39:50 +0100

    Found a replacement to coinflip.py
    I just tested around with chances

--- coinflip.py
+++
@@ -1,9 +0,0 @@
-import time, random
-chance = (random.randint(1,2))
-if chance == 1:
-    print "head"
-if chance == 2:
-    print "tail"
-time.sleep(5)
-# I created this program while I had forgotton that CobraConsole is a bat
-# and not a python file.
